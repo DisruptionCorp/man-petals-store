1a405c5d0e1ed75c8f4a258480cf9d9258817fcb
areis89@gmail.com <ec2-user@ip-172-31-18-126.ec2.internal>
2018-10-24 14:00:42 +0000

    /api/users CRUD routes created

---
+++ server/routes/api/users.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser')
+const { conn, Product, Order, LineItem, User, seed } = require('../../db/index')
+
+router.use(bodyParser.json())
+
+//routes begin with /api/users/
+
+//all users
+router.get('/', (req, res, next) => {
+    User
+        .findAll()
+        .then(users => { res.send(users) })
+        .catch(next)
+})
+
+//add user
+router.post('/api/users/', (req, res, next) => {
+    const { name } = req.body
+    User
+        .create({ name })
+        .then(user => res.json(user))
+        .catch(next)
+})
+
+
+//delete user, send back all users
+router.post('/api/users/:userId', (req, res, next) => {
+    User
+        .destroy({
+            where: {
+                id: req.params.userId
+            }
+        })
+        .findAll()
+        .then(users => { res.send(users) })
+        .catch(next)
+})
+
+//update user
+router.put('/api/usrs/:userId', (req, res, next) => {
+    User
+        .findById(req.params.id)
+        .then(user => { user.update(req.body) })
+        .then(user => { res.send(user) })
+        .catch(next)
+})
+
+
+module.exports = router
