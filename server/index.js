fd30211d717d41a26e566111084a1739966dc981
Sanjai S <sanjai.syamaprasad@gmail.com>
2018-10-25 06:03:56 -0400

    moved index.html and adjusted app.listen

--- server/index.js
+++ server/index.js
@@ -7,13 +7,17 @@ const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(process.cwd(), 'public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log("it's....").then(() => {
-    setTimeout(() => {
-      console.log('....alive.');
-    }, 2000);
-  });
-});
+// app.listen(port, () => {
+//   console.log("it's....").then(() => {
+//     setTimeout(() => {
+//       console.log('....alive.');
+//     }, 2000);
+//   });
+// });
+//****giving error- Please check Kevin****//
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
