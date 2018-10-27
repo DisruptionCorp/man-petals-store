74e6c3b44abef68eec1958026a1157ad83148304
Kevin Hu <pkvinhu@gmail.com>
2018-10-27 12:33:43 -0400

    added /POST to destroy, /PUT to update, and error handling tests for products

--- server/testing/product-api.spec.js
+++ server/testing/product-api.spec.js
@@ -26,14 +26,13 @@ describe('Product Route', () => {
   	  });
   	});
 
-  	it('/POST responds with 200, creates new task, and responds with new task', async () => {
+  	it('/POST responds with 200, creates new product, and responds with new product', async () => {
   	  await supertest
   	  .post('/api/products')
   	  .send({ name: 'grapefruit'})
   	  .expect(200)
   	  .expect('Content-Type', /json/)
   	  .expect(res => {
-  	  	console.log(res.body)
   	  	expect(res.body.name).to.eql('grapefruit');
   	  });
   	});
@@ -42,7 +41,58 @@ describe('Product Route', () => {
   describe('/api/products/:id', () => {
 
   	it('/POST responds with 200, destroys product, and responds with all products after destruction', async () => {
-  	  
-  	})
+  	  const [ adidas, nike, puma ] = await Promise.all([
+  	    Product.create({ name: 'adidas', price: 89.99 }),
+  	    Product.create({ name: 'nike', price: 59.99 }),
+  	    Product.create({ name: 'puma', price: 69.99 })
+  	  ]);
+  	  await supertest
+  	  .post(`/api/products/${adidas.id}`)
+  	  .expect(200)
+  	  .expect('Content-Type', /json/)
+  	  .expect(res => {
+  	  	const ids = res.body.reduce((acc, curr) => {
+  	  	  return [...acc, curr.id]
+  	  	}, []);
+  	  	expect(ids).to.not.include(adidas.id);
+  	  });
+  	});
+
+    it('/PUT responds with 200, updates product, and responds with updated product', async () => {
+      const [ uniqlo, muji, asos ] = await Promise.all([
+  	    Product.create({ name: 'uniqlo shirt'}),
+  	    Product.create({ name: 'muji shirt'}),
+  	    Product.create({ name: 'asos shirt'})
+  	  ]);
+      await supertest
+      .put(`/api/products/${uniqlo.id}`)
+      .send({ name: 'uniqlo shoes' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(res => {
+      	Product.findById(uniqlo.id)
+      	.then(updated => {
+      	  console.log(updated)
+      	  expect(updated.name).to.equal('uniqlo shoes')
+      	});
+      });
+    });
+  });
+
+  describe('error handling', () => {
+
+    it('responds with a 404 if a product does not exist', () => {
+      return supertest
+        .get('/api/products/6')
+        .expect(404);
+      });
+
+    it('responds with a 400 if you attempt to add a product without content', () => {
+      return supertest
+        .post('/api/products')
+        .send({ name: '' })
+        .expect(500);
+    });
+
   });
 });
