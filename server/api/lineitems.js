034e2f5955d1ed450df1fdc48445a819308401f0
Kevin Hu <pkvinhu@gmail.com>
2018-10-28 22:11:53 -0400

    added separate lineitems routes, with lineItemRouter

---
+++ server/api/lineitems.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const { LineItem, Order, Product } = require('../../db');
+
+
+// create line items
+router.post('/', (req, res, next) => {
+  LineItem
+  		.create()
+  		.then(item => {
+  		  res.send(item)
+  		});
+})
+
+// delete line items
+router.post('/:id', async (req, res, next) => {
+  await LineItem
+        .destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+  await LineItem.findAll()
+        .then(lineitems => { res.send(lineitems) })
+        .catch(next)
+})
+
+// update line items
+router.put('/:id', (req, res, next)=> {
+    LineItem
+		.update(req.body, { 
+			    where: { id: req.params.id }, 
+  	  		    returning: true, 
+  	  		    plain: true
+  	  		    })
+        .then(([ numRows, updated ]) => { 
+      	  res.send(updated)
+        })
+        .catch(next);
+  });
+
+module.exports = router;
