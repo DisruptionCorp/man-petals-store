b0e3102baa3df5a755e0d2e5e1103222766607ff
Kevin Hu <pkvinhu@gmail.com>
2018-10-30 13:19:58 -0400

    tweaked toggle function

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -20,7 +20,7 @@ export default class Navbar extends Component {
     }
 
     toggle(){
-      this.setState({ open: true})
+      this.setState({ open: !this.state.open})
     }
 
     handleClickAway(){
