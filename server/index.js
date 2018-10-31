1540fc06612680bf3691c36106ee6bfb5ec28a82
Kevin Hu <pkvinhu@gmail.com>
2018-10-31 12:13:41 -0400

    added auth.js api file and sessions middleware

--- server/index.js
+++ server/index.js
@@ -2,28 +2,46 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const path = require('path');
+const session = require('express-session');
+
+
+//File Imports
+const { sync, seed } = require('../db/index');
+
+
+//Router Imports
 const userRouter = require('./api/users');
 const productRouter = require('./api/products');
 const orderRouter = require('./api/orders');
 const lineItemsRouter = require('./api/lineitems');
 const reviewsRouter = require('./api/reviews');
-const port = process.env.PORT || 3000;
-const path = require('path');
-const { sync, seed } = require('../db/index');
+const authRouter = require('./api/auth');
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static(path.join(process.cwd(), 'public')));
-app.use('/public', express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.json());
 
+//Middleware
+app.use(morgan('dev'));     //logging
+app.use(express.json());    //body-parsing
+app.use(express.urlencoded());      //body-parsing   
+app.use(session({                           //session
+  secret: 'keep it secret, keep it safe',
+  resave: false,
+  saveUninitialized: false
+}));  
+app.use('/public', express.static(path.join(__dirname, '../public')));      //static     
+
+
+//Routers
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/lineitems', lineItemsRouter);
-app.use('api/reviews', reviewsRouter);
+app.use('/api/reviews', reviewsRouter);
+app.use('/api/auth', authRouter);
 
+
+//DB Sync
 sync()
   .then(() => {
     console.log('Database synced..');
