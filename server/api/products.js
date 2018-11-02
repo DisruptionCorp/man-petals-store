0c930dec352c9135a82f6b6f445daee4014b35df
Sanjai S <sanjai.syamaprasad@gmail.com>
2018-11-02 00:07:44 -0400

    cleaned up productReducer and api/products except post search tags

--- server/api/products.js
+++ server/api/products.js
@@ -1,70 +1,60 @@
 const Sequelize = require('sequelize');
 const express = require('express');
 const router = express.Router();
-const { Product, Review, LineItem, User } = require('../../db/index')
-
+const { Product, Review, LineItem, User } = require('../../db/index');
 
 //routes begin with /api/product/
 
 //all products
 router.get('/', (req, res, next) => {
-    Product
-        .findAll({ include: [  Review ]})
-        .then(products => { res.send(products) })
-        .catch(next)
-})
+  Product.findAll({ include: [Review] })
+    .then(products => res.send(products))
+    .catch(next);
+});
 
 //add product
 router.post('/', (req, res, next) => {
-    const { name } = req.body
-    Product
-        .create({ name })
-        .then(product => res.json(product))
-        .catch(next)
-})
+  Product.create(req.body)
+    .then(product => res.send(product))
+    .catch(next);
+});
 
-
-//delete product, send back all products
-router.post('/:id', async (req, res, next) => {
-    await Product
-        .destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-    await Product
-        .findAll()
-        .then(products => { res.send(products) })
-        .catch(next)
-})
+//delete product
+router.delete('/:id', (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(product => product.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(next);
+});
 
 //update product
 router.put('/:id', (req, res, next) => {
-    Product
-        .findById(req.params.id)
-        .then(product => product.update(req.body))
-        .then(product => res.send(product))
-        .catch(next)
-})
+  Product.findById(req.params.id)
+    .then(product => product.update(req.body))
+    .then(product => res.send(product))
+    .catch(next);
+});
 
 //search products by tags
 router.post('/search/tags', (req, res, next) => {
   const Op = Sequelize.Op;
 
-  Product
-        .findAll({ where: {
-          tags: {
-            [Op.contains] : [ ...req.body.tags.reduce((acc,each)=>{
-                  console.log(each);
-                  return [...acc, each]
-                }, []) ]
-          }
-        }})
-        .then(products => {
-          if(products) res.send(products)
-        })
-        .catch(next);
-})
+  Product.findAll({
+    where: {
+      tags: {
+        [Op.contains]: [
+          ...req.body.tags.reduce((acc, each) => {
+            console.log(each);
+            return [...acc, each];
+          }, []),
+        ],
+      },
+    },
+  })
+    .then(products => {
+      if (products) res.send(products);
+    })
+    .catch(next);
+});
 
-
-module.exports = router
+module.exports = router;
