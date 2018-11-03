be8829ea25412fa624cd0b7642d39ba0e797eb8a
Andrew Reis <areis89@gmail.com>
2018-11-03 12:20:33 -0400

    reviews

--- db/models/Review.js
+++ db/models/Review.js
@@ -3,14 +3,23 @@ const conn = require('../conn');
 const faker = require('faker');
 
 const Review = conn.define('review', {
+  id: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
   content: {
-  	type: Sequelize.TEXT,
-  	allowNull: false,
-  	//defaultValue: faker.Lorem.paragraph(),
-  	validate: {
-  	  notEmpty: true
-  	},
-  }
+    type: Sequelize.TEXT,
+    allowNull: false,
+    //defaultValue: faker.Lorem.paragraph(),
+    validate: {
+      notEmpty: true,
+    },
+  },
+  rating: {
+    type: Sequelize.INTEGER,
+    allowNull: true,
+  },
 });
 
 module.exports = Review;
--- server/api/reviews.js
+++ server/api/reviews.js
@@ -3,39 +3,36 @@ const router = express.Router();
 const { Review, Product, User } = require('../../db');
 
 // create a review for a product
-router.post('/', (res, req, next) => {
-  Review
-    .create(req.body)
+router.post('/', (req, res, next) => {
+  Review.create(req.body)
     .then(review => {
-      res.send(review)
+      res.send(review);
     })
     .catch(next);
-})
+});
 
 // delete a review by id
-router.delete('/:id', (res, req, next) => {
-  Review
-    .destroy({
-      where: {
-        id: req.params.id
-      }
-    })
+router.delete('/:id', (req, res, next) => {
+  Review.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
     .then(() => res.sendStatus(404).end())
     .catch(next);
 });
 
 // update a review by id
 router.post('/:id', (req, res, next) => {
-  Review
-    .update(req.body, {
-      where: { id: req.params.id },
-      returning: true,
-      plain: true
-    })
+  Review.update(req.body, {
+    where: { id: req.params.id },
+    returning: true,
+    plain: true,
+  })
     .then(([numRows, updated]) => {
-      res.send(updated)
+      res.send(updated);
     })
     .catch(next);
-})
+});
 
 module.exports = router;
